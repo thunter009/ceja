ffa051b8585b9002a4b9f6939064a9df44473175
Tom Hunter <tom@markerr.com>
2021-10-29 23:45:11 -0400

    add type hints

--- ceja/functions.py
+++ ceja/functions.py
@@ -1,55 +1,56 @@
 from pyspark.sql.types import StringType, IntegerType, BooleanType, FloatType
 from pyspark.sql.functions import udf, col
+from typing import Optional
 import jellyfish as J
 
 
 # PHONETIC ALGORITHMS
 
 @udf(returnType=StringType())
-def nysiis(s):
-     return None if s == None else J.nysiis(s)
+def nysiis(s: str) -> Optional[str]:
+    return None if s == None else J.nysiis(s)
 
 
 @udf(returnType=StringType())
-def metaphone(s):
-     return None if s == None else J.metaphone(s)
+def metaphone(s: str) -> Optional[str]:
+    return None if s == None else J.metaphone(s)
 
 
 @udf(returnType=StringType())
-def match_rating_codex(s):
-     return None if s == None else J.match_rating_codex(s)
+def match_rating_codex(s: str) -> Optional[str]:
+    return None if s == None else J.match_rating_codex(s)
 
 
 # STEMMING ALGORITHMS
 
 @udf(returnType=StringType())
-def porter_stem(s):
-     return None if s == None else J.porter_stem(s)
+def porter_stem(s: str) -> Optional[str]:
+    return None if s == None else J.porter_stem(s)
 
 
 # STRING COMPARISON
 
 @udf(returnType=IntegerType())
-def damerau_levenshtein_distance(s1, s2):
-     return None if s1 == None or s2 == None else J.damerau_levenshtein_distance(s1, s2)
+def damerau_levenshtein_distance(s1: str, s2: str) -> Optional[int]:
+    return None if s1 == None or s2 == None else J.damerau_levenshtein_distance(s1, s2)
 
 
 @udf(returnType=IntegerType())
-def hamming_distance(s1, s2):
-     return None if s1 == None or s2 == None else J.hamming_distance(s1, s2)
+def hamming_distance(s1: str, s2: str) -> Optional[int]:
+    return None if s1 == None or s2 == None else J.hamming_distance(s1, s2)
 
 
 @udf(returnType=FloatType())
-def jaro_similarity(s1, s2):
-     return None if s1 == None or s2 == None else J.jaro_similarity(s1, s2)
+def jaro_similarity(s1: str, s2: str) -> Optional[float]:
+    return None if s1 == None or s2 == None else J.jaro_similarity(s1, s2)
 
 
 @udf(returnType=FloatType())
-def jaro_winkler_similarity(s1, s2):
-     return None if s1 == None or s2 == None else J.jaro_winkler_similarity(s1, s2)
+def jaro_winkler_similarity(s1: str, s2: str) -> Optional[float]:
+    return None if s1 == None or s2 == None else J.jaro_winkler_similarity(s1, s2)
 
 
 @udf(returnType=BooleanType())
-def match_rating_comparison(s1, s2):
-     return None if s1 == None or s2 == None else J.match_rating_comparison(s1, s2)
+def match_rating_comparison(s1: str, s2: str) -> Optional[bool]:
+    return None if s1 == None or s2 == None else J.match_rating_comparison(s1, s2)
 
